525477d74390b0642a0ad16203a7362e4449a4c4
NgaSuPyae <moepokoe@googlemail.com>
2023-12-17 17:02:16 +0630

    Update add.js

--- add.js
+++ add.js
@@ -1,7 +1,7 @@
-let now_playing = document.querySelector('.now-playing');
 let track_art = document.querySelector('.track-art');
 let track_name = document.querySelector('.track-name');
 let track_artist = document.querySelector('.track-artist');
+
 
 let playpause_btn = document.querySelector('.playpause-track');
 let next_btn = document.querySelector('.next-track');
@@ -58,7 +58,7 @@ function loadTrack(track_index){
     track_art.style.backgroundImage = "url(" + music_list[track_index].img + ")";
     track_name.textContent = music_list[track_index].name;
     track_artist.textContent = music_list[track_index].artist;
-    now_playing.textContent = "အောဝ်း ";
+    
 
     updateTimer = setInterval(setUpdate, 1000);
 
